438f0918a76cfbb0cd667e33c0bd2141f467a076
Rifiya <57045022+Rifiya@users.noreply.github.com>
2026-09-02 19:58:58 +0530

    Create proc_load_silver.sql

---
+++ scripts/silver/proc_load_silver.sql
@@ -0,0 +1,9 @@
+/*
+==============================================================================================
+Stored Procedure: Load Silver Layer (Bronze-> Silver)
+==============================================================================================
+
+Script Purpose:
+  This stored procedure performs the ETL process to populate the silver schema tables from the bronze schema.
+  Actions Performed:
+    - Truncates silver tables
