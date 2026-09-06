0576b3f2d72d3ad6b16b0efc6aa8cf08dff5a9b9
Rifiya <57045022+Rifiya@users.noreply.github.com>
2026-09-06 12:06:12 +0530

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -1,1 +1,17 @@
 
+/*
+===========================================================================================
+DDL SCRIPT: Create Gold Views
+==========================================================================================
+Script Purpose: 
+      This sript creates views for the gold layer in the data warehouse.
+      The gold layer represents the final dimension and the fact tables (Star Schema)
+
+      Each view performs transformations and combines data from the silver layer to produce
+      a clean, enriched, and business-ready dataset.
+
+Usage:
+      These views can be queried directly for analytics and reporting.
+==========================================================================================
+*/
+
